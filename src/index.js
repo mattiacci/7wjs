1801e70621d088d8c277f8fae070b2c20ecd5148
Michael <mattiacci@gmail.com>
2017-05-08 23:00:56 -0700

    Add better error messaging when no .env is present

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+let firebaseConfigValid = false;
+try {
+  JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+  firebaseConfigValid = true;
+} catch (e) {
+  console.error('Invalid REACT_APP_FIREBASE_CONFIG:', e.message);
+}
+if (firebaseConfigValid) {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
